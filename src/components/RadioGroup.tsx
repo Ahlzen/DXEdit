30208f81e08744f1b14ec798961fb6ebaeca7e07
Lars Ahlzen <lars@ahlzen.com>
2026-06-07 15:02:25 +0100

    Added missing RadioGroup control.

---
+++ src/components/RadioGroup.tsx
@@ -0,0 +1,25 @@
+export default function RadioGroup(props: {
+  title: string,
+  options: {}, // value: name
+  selectedValue: number|null,
+  onValueChanged: (value: number) => void})
+{
+  return (
+    <div className="radio-group">
+      <label>{props.title}
+      {Object.entries(props.options).map(([value, name]) => (
+        <label key={value}>
+          <input
+            type="radio"
+            value={value}
+            name={props.title}
+            defaultChecked={props.selectedValue === Number(value)}
+            onChange={(e) => props.onValueChanged(Number(e.target.value))} />
+          {name}
+        </label>
+      ))} 
+      </label>
+    </div>
+  )
+
+};
